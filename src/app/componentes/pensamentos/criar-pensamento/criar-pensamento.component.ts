c488e432c39cbd219180184493c637c4974c969c
Rodrigo Brunet <rodrigosistemas2016@gmail.com>
2023-11-21 17:46:22 -0300

    desabilitando botao

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,42 +7,60 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
-  styleUrls: ['./criar-pensamento.component.css']
+  styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent implements OnInit {
-
   pensamento: Pensamento = {
     conteudo: '',
     autoria: '',
-    modelo: 'modelo1'
-  }
+    modelo: 'modelo1',
+  };
 
   formulario!: FormGroup;
 
-  constructor(private service: PensamentoService,
+  constructor(
+    private service: PensamentoService,
     private router: Router,
     private formBuilder: FormBuilder
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo: ['', [Validators.required]],
-      autoria: ['', [Validators.required]],
-      modelo: ['modelo1']
-    })
+      conteudo: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/),
+        ]),
+      ],
+      autoria: [
+        '',
+        Validators.compose([Validators.required,
+          Validators.minLength(3)
+        ]),
+      ],
+      modelo: ['modelo1'],
+    });
   }
 
-  criarPensamento(){
+  criarPensamento() {
+    if (this.formulario.valid) {
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento']);
+      });
+    }
+  }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento']);
+  }
+
+  habilitarBotao(): string{
     if(this.formulario.valid){
-      this.service.criar(this.formulario.value).subscribe(()=>{
-        this.router.navigate(['/listarPensamento'])
-       });
+      return 'botao'
+    }else{
+      return 'botao__desabilitado'
     }
 
   }
-
-  cancelar(){
-    this.router.navigate(['/listarPensamento'])
-  }
-
 }
